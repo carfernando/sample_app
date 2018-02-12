f5ff164e55296e06c1efeeccdd2a3a3b73e22f59
EC2 Default User <carlosfe.henrique@gmail.com>
2018-02-12 06:34:13 +0000

    Completing adding the full title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  def full_title(page_title="")
+  def full_title(page_title="")  #comment testing.. 
     base_title="Ruby on Rails Tutorial Sample App"
     if page_title.empty?
       base_title
