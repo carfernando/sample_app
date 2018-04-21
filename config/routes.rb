03bd161cbb128ae2091bca4f2df601f9b07caff1
EC2 Default User <carlosfe.henrique@gmail.com>
2018-04-21 20:30:31 +0000

    finishing layout add

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
 
-  get 'static_pages/home'
+  root 'static_pages#home'
 
-  get 'static_pages/help'
+  #get 'static_pages/help'
+  get "/help", to:"static_pages#help"  #, as: "helf"   ... I could rename the route to my convenience if i wanted it to look prettier on the browser when asking for the route.
   
-  get "static_pages/about"
+  #get "static_pages/about"
+  get "/about", to:"static_pages#about"
   
-  get "static_pages/contact"
-  
-  
+  #get "static_pages/contact"
+  get "/contact", to:"static_pages#contact"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "static_pages#home"
 end
